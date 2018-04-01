845a10bc67c20acfcdeb42694bc07465a36955c8
David S. Li <ds.li@outlook.com>
2018-03-31 21:41:08 -0400

    Adding PRIMARY KEY and CHECK clauses

--- hw2.sql
+++ hw2.sql
@@ -1,8 +1,8 @@
-CREATE TABLE trip(round_trip BOOLEAN, departure DATE, return DATE, locId INTEGER, region VARCHAR());
-CREATE TABLE location(visitorAmt INTEGER, locId INTEGER, region VARCHAR());
-CREATE TABLE group(groupId INTEGER);
-CREATE TABLE passenger(pasId INTEGER, age INTEGER, name VARCHAR());
-CREATE TABLE accommodation(bookingSource VARCHAR(), accId INTEGER, rate FLOAT);
-CREATE TABLE attraction(price INTEGER, thingsToDo VARCHAR());
-CREATE TABLE review();
-CREATE TABLE payment();
+CREATE TABLE trip(tripId INTEGER, round_trip BOOLEAN, departure DATE, return DATE, locId INTEGER, region VARCHAR(), PRIMARY KEY(tripId), CHECK(tripId > 0 AND return >= departure));
+CREATE TABLE location(visitorAmt INTEGER, locId INTEGER, region VARCHAR(), PRIMARY KEY(locId), CHECK(locId > 0));
+//CREATE TABLE group(groupId INTEGER);
+CREATE TABLE passenger(pasId INTEGER, age INTEGER, name VARCHAR(), PRIMARY KEY(pasId), CHECK(COUNT(*) > 0));
+CREATE TABLE accommodation(bookingSource VARCHAR(), accId INTEGER, rate FLOAT, PRIMARY KEY(accId), CHECK(accId > 0 AND rate > 0));
+CREATE TABLE attraction(attId INTEGER, price INTEGER, thingsToDo VARCHAR(), PRIMARY KEY(attId));
+CREATE TABLE review(rating INTEGER, Id INTEGER, PRIMARY KEY(Id), CHECK(rating > 0));
+CREATE TABLE payment(amount INTEGER, Id INTEGER, PRIMARY KEY (Id), CHECK(amount > 0));
