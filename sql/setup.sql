e909b62c4fb52c342cd35e7a2d64eac8090be3f8
Mary Taft <Mary.Taft@stonybrook.edu>
2018-05-06 17:40:57 -0400

    starting to finalize schemas, as well as inserting dummy data

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,8 +4,8 @@ go
 
 /* ENTITIES */
 
-CREATE TABLE users (
-       id INT AUTO_INCREMENT, --is NOT NULL necessary? todo: add just in case
+CREATE TABLE user (
+       id INT NOT NULL AUTO_INCREMENT,
        username VARCHAR(40),
        email VARCHAR(35) NOT NULL,
        password VARCHAR(20) NOT NULL,
@@ -13,89 +13,83 @@ CREATE TABLE users (
        UNIQUE (email)
        );
 
---todo: why do I even have this table (below)
-CREATE TABLE bookings (
-       id INT AUTO_INCREMENT,
-       startDate DATE NOT NULL,
-       endDate DATE,			--null for one-way transportation
-       PRIMARY KEY (id)
-       );
-
-CREATE TABLE transportation (
-       id INT AUTO_INCREMENT,
-       fare DOUBLE NOT NULL,
-       sourceLocation INT NOT NULL,
-       destinationLocation INT NOT NULL,
-       --departureTime DATETIME NOT NULL, --ONLY FOR CHAINED..?? figure out later
-       --arrivalTime DATETIME NOT NULL,
-       PRIMARY KEY (id),
-       FOREIGN KEY (sourceLocation) references locations(id),
-       FOREIGN KEY (destinationLocation) references locations(id),
-       CHECK (departureTime < arrivalTime),	--TODO not sure if this is correct
-       CHECK (fare > 0)
-       );
-
-/* NOTE: if we have 3 travel classes, does that mean each flight has 3 entries in the table? one for each class? is there a better way to do this? */
-CREATE TABLE flights (		--subentity of transportation
-       id INT NOT NULL,		--not sure the NOT NULL is necessary here; will figure out later, but shouldn't hurt regardless
-       airline VARCHAR(30) NOT NULL,
-       --flightClass ENUM('economy', 'business', 'first'),
-       numSeatsRemainingEconomy INT NOT NULL,
-       numSeatsRemainingBusiness INT NOT NULL,
-       numSeatsRemainingFirst INT NOT NULL,
-       FOREIGN KEY (id) REFERENCES transportation(id)
-       );
-
-CREATE TABLE trains (		--subentity of transportation
-       id INT NOT NULL,		--same as above^
-       railroad VARCHAR(30),
-       --trainClass ENUM('coach', 'business', 'first'),	--see above^
-       numSeatsRemainingCoach INT NOT NULL,
-       numSeatsRemainingBusiness INT NOT NULL,
-       numSeatsRemainingFirst INT NOT NULL,
-      FOREIGN KEY (id) REFERENCES transportation(id)
-       );
-
-CREATE TABLE hotels (
-       id INT AUTO_INCREMENT,
-       dailyCost DOUBLE NOT NULL,
-       address VARCHAR(30),	--street address only
-       location INT NOT NULL,
-       PRIMARY KEY (id),
-       FOREIGN KEY (location) REFERENCES locations(id),
-       CHECK (dailyCost > 0)
-       --todo: remove assumption of infinite capacity / # of rooms..?
-       );
-
-CREATE TABLE locations (
-       id INT AUTO_INCREMENT,
+CREATE TABLE location (
+       id INT NOT NULL AUTO_INCREMENT,
        city VARCHAR(30) NOT NULL,
-       region VARCHAR(2),		--use standard region abbrevs.
-       country VARCHAR(2) DEFAULT 'US',	--use standard country abbrevs.
+       region VARCHAR(2),
+       country VARCHAR(2) DEFAULT 'US',
        PRIMARY KEY (id),
        UNIQUE (city, region, country)
        );
 
-CREATE TABLE attractions (
+CREATE TABLE booking (
+       id INT NOT NULL AUTO_INCREMENT,
+       startDate DATE NOT NULL,
+       PRIMARY KEY (id)
+       );       
+
+--departureTime DATETIME NOT NULL,
+--arrivalTime DATETIME NOT NULL,
+CREATE TABLE transportation (
+       id INT NOT NULL,
+       sourceLocation INT NOT NULL,
+       destinationLocation INT NOT NULL,
+       fareEconomy DECIMAL(6,2) NOT NULL,
+       fareBusiness DECIMAL(6,2) NOT NULL,
+       fareFirst DECIMAL(6,2) NOT NULL,
+       numSeatsRemainingEconomy INT NOT NULL,
+       numSeatsRemainingBusiness INT NOT NULL,
+       numSeatsRemainingFirst INT NOT NULL,
+       FOREIGN KEY (id) REFERENCES booking(id),
+       FOREIGN KEY (sourceLocation) REFERENCES location(id),
+       FOREIGN KEY (destinationLocation) REFERENCES location(id),
+       CHECK (departureTime < arrivalTime),
+       CHECK (fare > 0)
+       );
+
+CREATE TABLE flight (
+       id INT NOT NULL,
+       airline VARCHAR(30) NOT NULL,
+       FOREIGN KEY (id) REFERENCES transportation(id)
+       );
+
+CREATE TABLE train (
+       id INT NOT NULL,
+       railroad VARCHAR(30),
+       FOREIGN KEY (id) REFERENCES transportation(id)
+       );
+
+--note: assumes hotel has infinite capacity
+CREATE TABLE hotel (
+       id INT NOT NULL,
+       dailyCost DECIMAL(6,2) NOT NULL,
+       address VARCHAR(30),
+       location INT NOT NULL,
+       FOREIGN KEY (id) REFERENCES booking(id),
+       FOREIGN KEY (location) REFERENCES location(id),
+       CHECK (dailyCost > 0)
+       );
+
+CREATE TABLE payment (
        id INT AUTO_INCREMENT,
-       location INT NOT NULL, --might not be necessary given the 'featuresAttraction' relationship, but if we scrap that table this will suffice for queries (it's more of an efficiency thing than a necessity to have the separate table)
+       amount DECIMAL(6,2) NOT NULL,
+       paymentType ENUM('debit', 'credit'),
+       cardNo VARCHAR(16) NOT NULL,
+       PRIMARY KEY (id),
+       CHECK (amount > 0)
+       );
+
+CREATE TABLE attraction (
+       id INT AUTO_INCREMENT,
+       location INT NOT NULL,
        attractionName VARCHAR(30) NOT NULL,
-       image1 VARCHAR(30) DEFAULT 'tmp.jpg',	--we'll need a folder of images
+       image1 VARCHAR(30) DEFAULT 'tmp.jpg',
        image2 VARCHAR(30) DEFAULT 'tmp.jpg',
        image3 VARCHAR(30) DEFAULT 'tmp.jpg',
        PRIMARY KEY (id),
        FOREIGN KEY (location) REFERENCES locations(id)
        );
        
-CREATE TABLE payments (
-       id INT AUTO_INCREMENT,
-       amount DOUBLE NOT NULL,	--TODO: somehow make sure this is sum of travel+hotel costs + "service fee" for using our travel site (incentive: save money by booking a package of things)
-       paymentType ENUM('debit', 'credit', 'cash', 'check'),
-       --todo: add transactionDate here? instead of as an attribute of the 'books' relationship?
-       PRIMARY KEY (id),
-       CHECK (amount > 0)
-       );
-
 CREATE TABLE reviews (
        id INT AUTO_INCREMENT,
        numStars INT NOT NULL,
@@ -110,23 +104,16 @@ CREATE TABLE reviews (
 
 /* RELATIONSHIPS */
 
-/* don't need writesReview, just put an author field in reviews */
-
-CREATE TABLE books (
+ --for transactionDate: or DATETIME --p.s. can omit if we don't display it in purchase history
+CREATE TABLE purchase (
        userID INT,
        bookingID INT,
-       transactionDate DATETIME NOT NULL, --todo: move this to payment??
-       PRIMARY KEY (userID, bookingID),
-       FOREIGN KEY (userID) REFERENCES users(id),
-       FOREIGN KEY (bookingID) REFERENCES bookings(id)
-       );
-
-CREATE TABLE requiresPayment (	--todo: lol rename this probably..
-       bookingID INT,
        paymentID INT,
-       PRIMARY KEY (bookingID, paymentID),
-       FOREIGN KEY (bookingID) REFERENCES bookings(id),
-       FOREIGN KEY (paymentID) REFERENCES payments(id)
+       transactionDate DATE NOT NULL,
+       PRIMARY KEY (userID, bookingID, paymentID),
+       FOREIGN KEY (userID) REFERENCES user(id),
+       FOREIGN KEY (bookingID) REFERENCES booking(id),
+       FOREIGN KEY (paymentID) REFERENCES payment(id)
        );
 
 /* don't need headedTo/headedFrom/locatedIn, just put location fields in transportation and hotels */
@@ -138,3 +125,12 @@ CREATE TABLE featuresAttractions (
        FOREIGN KEY (locationID) REFERENCES locations(id),
        FOREIGN KEY (attractionID) REFERENCES attractions(id)
        );
+
+
+INSERT INTO location VALUES (1, 'nyc', 'ny', 'us');
+INSERT INTO location VALUES (2, 'sf', 'ca', 'us');
+INSERT INTO booking VALUES (1, '2018-05-27');
+INSERT INTO transportation VALUES (1, 1, 2, 330.45, 440.23, 605.88, 40, 20, 10);
+INSERT INTO flight VALUES (1, 'delta');
+
+SELECT 
