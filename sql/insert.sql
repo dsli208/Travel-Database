22d8e38601edb0bf79ff3b5f6a9162e11572d1cb
Mary Taft <Mary.Taft@stonybrook.edu>
2018-05-07 00:36:50 -0400

    insert stmts

---
+++ sql/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO travellite_user VALUES (
+
+INSERT INTO travellite_location VALUES ('New York City', 'NY', 'background1.jpg')
+INSERT INTO travellite_location VALUES ('Boston', 'MA', 'background2.jpg')
+
+INSERT INTO travellite_flight VALUES (
+
+INSERT INTO travellite_train VALUES (
+
+INSERT INTO travellite_hotel VALUES (
+
+INSERT INTO travellite_payment VALUES (
+
+INSERT INTO travellite_attraction VALUES ('New York City', 'Empire State Building', 'it is tall', 'background1.jpg')
+INSERT INTO travellite_attraction VALUES ('New York City', 'Brooklyn Bridge', 'it is long', 'background2.jpg')
+INSERT INTO travellite_attraction VALUES ('New York City', 'Statue of Liberty', 'it is liberated', 'background1.jpg')
+
+INSERT INTO travellite_history VALUES (
+
