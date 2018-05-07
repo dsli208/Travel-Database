bbf311115f695d2075e3f0cef7eb13facbebfa33
Mary Taft <Mary.Taft@stonybrook.edu>
2018-05-07 00:53:32 -0400

    inserted more things yay

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,7 +1,11 @@
 INSERT INTO travellite_user VALUES (
 
-INSERT INTO travellite_location VALUES ('New York City', 'NY', 'background1.jpg')
-INSERT INTO travellite_location VALUES ('Boston', 'MA', 'background2.jpg')
+INSERT INTO travellite_location VALUES (1, 'New York City', 'NY', 'NewYorkCity.jpg');
+INSERT INTO travellite_location VALUES (2, 'Chicago', 'IL', 'Chicago.jpg');
+INSERT INTO travellite_location VALUES (3, 'Los Angeles', 'CA', 'LosAngeles.jpg');
+INSERT INTO travellite_location VALUES (4, 'Boston', 'MA', 'Boston.jpg');
+INSERT INTO travellite_location VALUES (5, 'Las Vegas', 'NV', 'LasVegas.jpg');
+INSERT INTO travellite_location VALUES (6, 'Atlanta', 'GA', 'Atlanta.jpg');
 
 INSERT INTO travellite_flight VALUES (
 
@@ -11,9 +15,25 @@ INSERT INTO travellite_hotel VALUES (
 
 INSERT INTO travellite_payment VALUES (
 
-INSERT INTO travellite_attraction VALUES ('New York City', 'Empire State Building', 'it is tall', 'background1.jpg')
-INSERT INTO travellite_attraction VALUES ('New York City', 'Brooklyn Bridge', 'it is long', 'background2.jpg')
-INSERT INTO travellite_attraction VALUES ('New York City', 'Statue of Liberty', 'it is liberated', 'background1.jpg')
+INSERT INTO travellite_attraction VALUES (1, 'New York City', 'Empire State Building', 'todo', 'NewYorkCity1.jpg');
+INSERT INTO travellite_attraction VALUES (2, 'New York City', 'World Trade Center', 'todo', 'NewYorkCity2.jpg');
+INSERT INTO travellite_attraction VALUES (3, 'New York City', 'Central Park', 'todo', 'NewYorkCity3.jpg');
+INSERT INTO travellite_attraction VALUES (4, 'Chicago', 'Willis Tower', 'todo', 'Chicago1.jpg');
+INSERT INTO travellite_attraction VALUES (5, 'Chicago', 'John Hancock Center', 'todo', 'Chicago2.jpg');
+INSERT INTO travellite_attraction VALUES (6, 'Chicago', 'Millenium Park', 'todo', 'Chicago3.jpg');
+INSERT INTO travellite_attraction VALUES (7, 'Los Angeles', 'Wilshire Grand Center', 'todo', 'LosAngeles1.jpg');
+INSERT INTO travellite_attraction VALUES (8, 'Los Angeles', 'Rose Bowl', 'todo', 'LosAngeles2.jpg');
+INSERT INTO travellite_attraction VALUES (9, 'Los Angeles', 'Staples Center', 'todo', 'LosAngeles3.jpg');
+INSERT INTO travellite_attraction VALUES (10, 'Boston', 'Massachusetts State House', 'todo', 'Boston1.jpg');
+INSERT INTO travellite_attraction VALUES (11, 'Boston', 'Harvard University', 'todo', 'Boston2.jpg');
+INSERT INTO travellite_attraction VALUES (12, 'Boston', 'Faneiul Hall', 'todo', 'Boston3.jpg');
+INSERT INTO travellite_attraction VALUES (13, 'Las Vegas', 'Caesars Palace', 'todo', 'LasVegas1.jpg');
+INSERT INTO travellite_attraction VALUES (14, 'Las Vegas', 'High Roller', 'todo', 'LasVegas2.jpg');
+INSERT INTO travellite_attraction VALUES (15, 'Las Vegas', 'MGM Grand', 'todo', 'LasVegas3.jpg');
+INSERT INTO travellite_attraction VALUES (16, 'Atlanta', 'World of Coca Cola', 'todo', 'Atlanta1.jpg');
+INSERT INTO travellite_attraction VALUES (17, 'Atlanta', 'CNN Center', 'todo', 'Atlanta2.jpg');
+INSERT INTO travellite_attraction VALUES (18, 'Atlanta', 'Olympic Park', 'todo', 'Atlanta3.jpg');
+
 
 INSERT INTO travellite_history VALUES (
 
