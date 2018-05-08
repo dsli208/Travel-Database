5655c030276166b49fcbde2515b76e511d620820
Anthony Chan <anthonychan0123@gmail.com>
2018-05-07 23:53:21 -0400

    full app implementation

--- app/travellite/views.py
+++ app/travellite/views.py
@@ -5,6 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 import jwt
 import json
+import datetime
+
+from .models import User, Location, History, Flight, Train, Hotel, Payment, Attraction, Review
 
 json_file = open('databaseProj/config_vars.json').read()
 data = json.loads(json_file)
@@ -13,40 +16,93 @@ data = json.loads(json_file)
 def index(request):
 	if request.method == 'POST':
 		secret = data['secret']
+		source = request.POST['source']
+		sourceArr = source.split(',')
+		sourceCity = sourceArr[0];
+		destination = request.POST['destination']
+		destinationArr = destination.split(',')
+		destinationCity = destinationArr[0];
+		startdate = request.POST['startdate']
+		flightClass = request.POST['class']
+		flights = Flight.objects.filter(sourceLocation = sourceCity).filter(destinationLocation=destinationCity)
+		flights = list(flights)
 		print(request.POST)
-		return render(request, 'index.html',{"results":"ye"})
+		return render(request, 'index.html',{"results":"yes", "some_list": flights, "class":flightClass})
 	else:
-		return render(request, 'trains.html')
+		return render(request, 'index.html')
 	## figure out how booking will work (send to booking page?)
 	
 
 @csrf_exempt
 def login(request):
 	if request.method == 'POST':
-		#print(request.POST.get('email'))
-		# if email is in database and checks with password
-		if (True):
-			payload = {}
-			secret = data['secret']
-			encoded = jwt.encode(payload, secret, algorithm='HS256')
-			return render(request, 'index.html', {'session': encoded})
+		email = request.POST['email']
+		password = request.POST['password']
+		check_user = User.objects.filter(email = email)
+		valid_user = (len(list(check_user)) == 1)
+		if (valid_user):
+			current_user = email
+			user_name = check_user.first().username
+			request.session['current_user'] = current_user
+			request.session['user_name'] = user_name
+			#encoded = jwt.encode(payload, secret, algorithm='HS256')
+			return render(request, 'login.html', {'msg': 'Login successful'})
 		else:
-			return render(request, 'login.html')
+			return render(request, 'login.html', {'msg': 'Failed. Please try again'})
 	else:
 		return render(request, 'login.html')
 
+@csrf_exempt
 def signup(request):
-	return render(request, 'signup.html')
+	if request.method == 'POST':
+		username = request.POST['username']
+		email = request.POST['email']
+		password = request.POST['password']
+		print(request.POST)
+		existing_email = User.objects.filter(email = email)
+		is_new_user = (len(list(existing_email)) == 0)
+		print(is_new_user)
+		if (is_new_user):
+			new_user = User.objects.create(username=username, email=email, password=password)
+			new_user.save()
+			return render(request, 'signup.html', {'msg': 'Sign up successful'})
+		else:
+			return render(request, 'signup.html', {'msg': 'Error. Email already exists'})
+	else:
+		return render(request, 'signup.html')
+
+@csrf_exempt
+def reviews(request):
+	if request.method == 'POST':
+		review = request.POST['review']
+		rating = request.POST['rating']
+		author = request.session['user_name']
+		current_date = datetime.datetime.today().strftime('%Y-%m-%d')
+		user_exists = Review.objects.filter(author = author)
+		if (len(list(user_exists)) == 0):
+			new_review = Review.objects.create(review=review, rating=rating, submissionDate= current_date,author= author)
+		reviews = Review.objects.all()
+		return render(request, 'reviews.html', {'results': 'yes', 'some_list': reviews})
+	else:
+		reviews = Review.objects.all()
+		return render(request, 'reviews.html', {'results': 'yes', 'some_list': reviews})
 
 @csrf_exempt
 def hotels(request):
 	if request.method == 'POST':
 		secret = data['secret']
+		location = request.POST['location']
+		locationArr = location.split(',')
+		locationCity = locationArr[0];
+		startdate = request.POST['startdate']
+		enddate = request.POST['enddate']
+		hotels = Hotel.objects.filter(city = locationCity)
+		hotels = list(hotels)
 		#decoded = jwt.verify((request.session.token), data['secret']);
 		#valid = jwt.decode(encoded, secret, algorithms=['HS256'])
 		print(request.POST)
 		#return render(request, 'hotels.html', {'session': session})
-		return render(request, 'hotels.html',{"results":"ye"})
+		return render(request, 'hotels.html',{"results":"yes", "some_list": hotels})
 	else:
 		return render(request, 'hotels.html')
 	
@@ -54,8 +110,18 @@ def hotels(request):
 def trains(request):
 	if request.method == 'POST':
 		secret = data['secret']
+		source = request.POST['source']
+		sourceArr = source.split(',')
+		sourceCity = sourceArr[0];
+		destination = request.POST['destination']
+		destinationArr = destination.split(',')
+		destinationCity = destinationArr[0];
+		startdate = request.POST['startdate']
+		trainClass = request.POST['class']
+		trains = Train.objects.filter(sourceLocation = sourceCity).filter(destinationLocation=destinationCity)
+		trains = list(trains)
 		print(request.POST)
-		return render(request, 'trains.html',{"results":"ye"})
+		return render(request, 'trains.html',{"results":"yes", "some_list": trains, "class":trainClass})
 	else:
 		return render(request, 'trains.html')
 
@@ -63,16 +129,82 @@ def trains(request):
 def explore(request):
 	if request.method == 'POST':
 		secret = data['secret']
-		print(request.POST)
-		return render(request, 'explore.html',{"results":"ye"})
+		location = request.POST['location']
+		locationArr = location.split(',')
+		city = locationArr[0];
+		region = locationArr[1];
+		location = Location.objects.filter(city = city)
+		attraction = Attraction.objects.filter(city = city)
+		location = list(location)
+		return render(request, 'explore.html',{"results":"yes", "location": location, "some_list":attraction})
 	else:
 		return render(request, 'explore.html')
 
+def book(request):
+	if request.method == 'POST':
+		objId = request.GET.get('id')
+		bookType = request.GET.get('type')
+		card_number = request.POST['card_number']
+		card_type = request.POST['card_type']
+		current_user = request.session['current_user']
+		current_date = datetime.datetime.today().strftime('%Y-%m-%d')
+		if (bookType == 'flight'):
+			travelClass = request.GET.get('class')
+			obj = Flight.objects.filter(id = objId).first()
+			if (travelClass == 'economy'):
+				new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.fareEconomy, paymentCardNo=card_number, companyName=obj.companyName, location=obj.destinationLocation)
+				new_transaction.save()
+			elif (travelClass == 'business'):
+				new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.fareBusiness, paymentCardNo=card_number, companyName=obj.companyName, location=obj.destinationLocation)
+				new_transaction.save()
+			elif (travelClass == 'first'):
+				new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.fareFirst, paymentCardNo=card_number, companyName=obj.companyName, location=obj.destinationLocation)
+				new_transaction.save()
+		elif (bookType == 'train'):
+			travelClass = request.GET.get('class')
+			obj = Train.objects.filter(id = objId).first()
+			if (travelClass == 'economy'):
+				new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.fareEconomy, paymentCardNo=card_number, companyName=obj.companyName, location=obj.destinationLocation)
+				new_transaction.save()
+			elif (travelClass == 'business'):
+				new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.fareBusiness, paymentCardNo=card_number, companyName=obj.companyName, location=obj.destinationLocation)
+				new_transaction.save()
+			elif (travelClass == 'first'):
+				new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.fareFirst, paymentCardNo=card_number, companyName=obj.companyName, location=obj.destinationLocation)
+				new_transaction.save()
+		elif (bookType == 'hotel'):
+			obj = Hotel.objects.filter(id = objId).first()
+			new_transaction = History.objects.create(userEmail=current_user, bookingType=bookType, bookingStartDate= current_date,paymentAmount=obj.dailyCost, paymentCardNo=card_number, companyName=obj.companyName, location=obj.address)
+			new_transaction.save()
+		return render(request, 'book.html', {'msg': 'Booking successful'})
+	else:
+		objId = request.GET.get('id')
+		bookType = request.GET.get('type')
+		if (bookType == 'flight'):
+			travelClass = request.GET.get('class')
+			obj = Flight.objects.filter(id = objId)
+			return render(request, 'book.html', {'booking':'yes', 'some_list':obj, 'type':bookType, 'class': travelClass})
+		elif (bookType == 'train'):
+			travelClass = request.GET.get('class')
+			obj = Train.objects.filter(id = objId)
+			return render(request, 'book.html', {'booking':'yes', 'some_list':obj, 'type':bookType, 'class': travelClass})
+		elif (bookType == 'hotel'):
+			obj = Hotel.objects.filter(id = objId)
+			return render(request, 'book.html', {'booking':'yes', 'some_list':obj, 'type':bookType})
+		else:
+			return render(request, 'book.html')
+
 def account(request):
 	setting = request.GET.get('setting')
-	return render(request, 'account.html', {'setting': setting})
+	current_user = request.session['current_user']
+	if (setting == 'history'):
+		history = History.objects.filter(userEmail = current_user)
+		return render(request, 'account.html', {'setting': setting, 'transactions': history})
+	else:
+		return render(request, 'account.html', {'setting': setting})
 
 def logout(request):
 	#clear session
-	#request.session = None
-	return render(request, 'index.html')
+	del request.session['current_user']
+	del request.session['user_name']
+	return render(request, 'login.html', {'msg': 'Logout successful'})
