a3f51513511bf3e0846e8b47216846c2023bda89
Mary Rosaria Taft <Mary.Taft97@gmail.com>
2018-05-09 12:50:08 -0400

    flights done, hopefully

--- sql/insert.sql
+++ sql/insert.sql
@@ -7,25 +7,36 @@ INSERT INTO travellite_location VALUES (4, 'Boston', 'MA', 'Boston.jpg');
 INSERT INTO travellite_location VALUES (5, 'Las Vegas', 'NV', 'LasVegas.jpg');
 INSERT INTO travellite_location VALUES (6, 'Atlanta', 'GA', 'Atlanta.jpg');
 
-INSERT INTO travellite_flight VALUES (1, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-29', '15:30:00', 300.97, 503.22, 773.48, 40, 12, 2);
-INSERT INTO travellite_flight VALUES (2, 'JetBlue', 'New York City', 'Los Angeles', '2018-05-29', '19:00:00', 330.97, 573.22, 803.48, 35, 10, 4);
-INSERT INTO travellite_flight VALUES (3, 'Delta Air Lines', 'Boston', 'Los Angeles', '2018-12-25', '5:30:00', 200.97, 403.12, 533.48, 50, 16, 8);
-INSERT INTO travellite_flight VALUES (4, 'American Airlines', 'Las Vegas', 'Los Angeles', '2018-08-02', '19:45:00', 200.45, 403.22, 900.08, 40, 6, 10);
-INSERT INTO travellite_flight VALUES (5, 'American Airlines', 'Atlanta', 'Chicago', '2018-07-16', '03:15:00', 250.97, 503.22, 573.48, 40, 10, 3);
-INSERT INTO travellite_flight VALUES (6, 'Delta Air Lines', 'Atlanta', 'Chicago', '2018-07-16', '09:20:00', 300.88, 403.35, 619.99, 30, 9, 6);
---todo: all flights below this line
-INSERT INTO travellite_flight VALUES (7, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-29', '15:30:00', 300.97, 503.22, 773.48, 40, 12, 2);
-INSERT INTO travellite_flight VALUES (8, 'JetBlue', 'New York City', 'Los Angeles', '2018-05-29', '19:00:00', 330.97, 573.22, 803.48, 35, 10, 4);
-INSERT INTO travellite_flight VALUES (9, 'Delta Air Lines', 'Boston', 'Los Angeles', '2018-12-25', '5:30:00', 200.97, 403.12, 533.48, 50, 16, 8);
-INSERT INTO travellite_flight VALUES (10, 'American Airlines', 'Las Vegas', 'Los Angeles', '2018-08-02', '19:45:00', 200.45, 403.22, 900.08, 40, 6, 10);
-INSERT INTO travellite_flight VALUES (11, 'American Airlines', 'Atlanta', 'Chicago', '2018-07-16', '03:15:00', 250.97, 503.22, 573.48, 40, 10, 3);
-INSERT INTO travellite_flight VALUES (12, 'Delta Air Lines', 'Atlanta', 'Chicago', '2018-07-16', '09:20:00', 300.88, 403.35, 619.99, 30, 9, 6);
-INSERT INTO travellite_flight VALUES (13, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-29', '15:30:00', 300.97, 503.22, 773.48, 40, 12, 2);
-INSERT INTO travellite_flight VALUES (14, 'JetBlue', 'New York City', 'Los Angeles', '2018-05-29', '19:00:00', 330.97, 573.22, 803.48, 35, 10, 4);
-INSERT INTO travellite_flight VALUES (15, 'Delta Air Lines', 'Boston', 'Los Angeles', '2018-12-25', '5:30:00', 200.97, 403.12, 533.48, 50, 16, 8);
-INSERT INTO travellite_flight VALUES (15, 'American Airlines', 'Las Vegas', 'Los Angeles', '2018-08-02', '19:45:00', 200.45, 403.22, 900.08, 40, 6, 10);
-INSERT INTO travellite_flight VALUES (17, 'American Airlines', 'Atlanta', 'Chicago', '2018-07-16', '03:15:00', 250.97, 503.22, 573.48, 40, 10, 3);
-INSERT INTO travellite_flight VALUES (18, 'Delta Air Lines', 'Atlanta', 'Chicago', '2018-07-16', '09:20:00', 300.88, 403.35, 619.99, 30, 9, 6);
+-- 3 flights NY -> LA on 5/29
+-- 3 flights NY -> LA on 5/30
+-- 5 flights LA -> NY on 6/04
+-- 3 flights Atlanta -> Las Vegas on 07/03
+-- 2 flights Las Vegas -> Atlanta on 07/11
+-- 1 flights NY -> Boston on 09/19
+-- 2 flights Boston -> Chicago on 09/22
+-- 1 flights Chicago -> NY on 09/27
+
+INSERT INTO travellite_flight VALUES (1, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-29', '09:00:00', 111.00, 238.50, 577.00, 50, 19, 5);
+INSERT INTO travellite_flight VALUES (2, 'JetBlue', 'New York City', 'Los Angeles', '2018-05-29', '11:30:00', 109.00, 305.00, 462.50, 47, 22, 10);
+INSERT INTO travellite_flight VALUES (3, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-29', '15:30:00', 115.50, 283.00, 501.50, 69, 15, 8);
+INSERT INTO travellite_flight VALUES (4, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-30', '10:30:00', 102.00, 287.50, 560.00, 66, 20, 3);
+INSERT INTO travellite_flight VALUES (5, 'JetBlue', 'New York City', 'Los Angeles', '2018-05-30', '12:45:00', 99.00, 254.50, 490.00, 62, 12, 9);
+INSERT INTO travellite_flight VALUES (6, 'Delta Air Lines', 'New York City', 'Los Angeles', '2018-05-30', '17:00:00', 103.50, 273.00, 522.00, 50, 18, 11);
+INSERT INTO travellite_flight VALUES (7, 'American Airlines', 'Los Angeles', 'New York City', '2018-06-04', '05:30:00', 140.00, 302.00, 630.00, 10, 0, 1);
+INSERT INTO travellite_flight VALUES (8, 'American Airlines', 'Los Angeles', 'New York City', '2018-06-04', '10:00:00', 137.00, 300.00, 623.00, 5, 2, 0);
+--book the one below?
+INSERT INTO travellite_flight VALUES (9, 'Delta Air Lines', 'Los Angeles', 'New York City', '2018-06-04', '12:45:00', 122.00, 297.00, 580.00, 1, 0, 0);
+INSERT INTO travellite_flight VALUES (10, 'Delta Air Lines', 'Los Angeles', 'New York City', '2018-06-04', '14:30:00', 129.50, 296.50, 620.00, 18, 7, 6);
+INSERT INTO travellite_flight VALUES (11, 'JetBlue', 'Los Angeles', 'New York City', '2018-06-04', '16:45:00', 148.00, 312.50, 640.50, 10, 4, 4);
+INSERT INTO travellite_flight VALUES (12, 'JetBlue', 'Atlanta', 'Las Vegas', '2018-07-03', '12:30:00', 99.00, 205.00, 392.50, 67, 21, 10);
+INSERT INTO travellite_flight VALUES (13, 'American Airlines', 'Atlanta', 'Las Vegas', '2018-07-03', '19:30:00', 105.50, 203.00, 410.00, 69, 16, 4);
+INSERT INTO travellite_flight VALUES (14, 'Delta Air Lines', 'Atlanta', 'Las Vegas', '2018-07-03', '22:30:00', 94.00, 187.50, 460.00, 58, 20, 12);
+INSERT INTO travellite_flight VALUES (15, 'JetBlue', 'Las Vegas', 'Atlanta', '2018-07-11', '12:15:00', 90.00, 154.00, 410.50, 72, 16, 9);
+INSERT INTO travellite_flight VALUES (16, 'Delta Air Lines', 'Las Vegas', 'Atlanta', '2018-07-11', '16:00:00', 93.50, 172.00, 422.00, 61, 18, 14);
+INSERT INTO travellite_flight VALUES (17, 'American Airlines', 'New York City', 'Boston', '2018-09-19', '12:30:00', 95.50, 203.00, 422.00, 79, 16, 4);
+INSERT INTO travellite_flight VALUES (18, 'Delta Air Lines', 'Boston', 'Chicago', '2018-09-22', '12:30:00', 94.00, 187.50, 436.00, 78, 10, 12);
+INSERT INTO travellite_flight VALUES (19, 'JetBlue', 'Boston', 'Chicago', '2018-09-22', '23:15:00', 93.00, 154.00, 410.50, 72, 16, 3);
+INSERT INTO travellite_flight VALUES (20, 'Delta Air Lines', 'Chicago', 'New York City', '2018-09-27', '16:30:00', 93.50, 172.00, 422.00, 69, 19, 14);
 
 INSERT INTO travellite_train VALUES (1, 'Amtrak', 'New York City', 'Boston', '2018-05-29', '15:30:00', 103.90, 203.22, 273.48, 240, 112, 53);
 INSERT INTO travellite_train VALUES (2, 'Amtrak', 'New York City', 'Boston', '2018-06-04', '15:30:00', 83.90, 128.14, 203.43, 312, 362, 62);
